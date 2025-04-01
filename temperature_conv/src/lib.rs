9989be3f7c18b4f79c1447d783eb258dc2417a60
abrakingoo <maikoabraham68@gmail.com>
2025-04-01 19:07:42 +0300

    lib: temp_conv

--- temperature_conv/src/lib.rs
+++ temperature_conv/src/lib.rs
@@ -1,9 +1,7 @@
 pub fn fahrenheit_to_celsius(f: f64) -> f64 {
-    // Convert from Fahrenheit to Celsius, rounding to 6 decimal places
     ((f - 32.0) * 5.0 / 9.0 * 1_000_000.0).round() / 1_000_000.0
 }
 
 pub fn celsius_to_fahrenheit(c: f64) -> f64 {
-    // Convert from Celsius to Fahrenheit, rounding to 6 decimal places
-    ((c * 9.0 / 5.0) + 32.0) * 1_000_000.0.round() / 1_000_000.0
+    ((c * 9.0 / 5.0) + 32.0) * 1_000_000.0_f64.round() / 1_000_000.0_f64
 }
