e739dda1993d0255d025e2f116ded22a39421004
abrakingoo <maikoabraham68@gmail.com>
2025-04-01 18:01:45 +0300

    lib: scalar

--- variables/scalar/target/debug/deps/scalar-e5a77a3b9c09004d.d
+++
@@ -1,5 +0,0 @@
-/home/abu/piscine-rust/variables/scalar/target/debug/deps/scalar-e5a77a3b9c09004d: src/lib.rs
-
-/home/abu/piscine-rust/variables/scalar/target/debug/deps/scalar-e5a77a3b9c09004d.d: src/lib.rs
-
-src/lib.rs:
