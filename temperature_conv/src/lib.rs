f1ef142bac8f356f152cef1737cd5b4d0c3ed6db
abrakingoo <maikoabraham68@gmail.com>
2025-04-01 18:48:50 +0300

    lib: temp_conv

--- temperature_conv/src/lib.rs
+++ temperature_conv/src/lib.rs
@@ -5,13 +5,3 @@ pub fn fahrenheit_to_celsius(f: f64) -> f64 {
 pub fn celsius_to_fahrenheit(c: f64) -> f64 {
     ((c * 9.0 / 5.0) + 32.0).round() / 100.0
 }
-
-fn main() {
-    let fahrenheit = 20.0;
-    let celsius = fahrenheit_to_celsius(fahrenheit);
-    println!("{}°F is {}°C", fahrenheit, celsius);
-
-    let celsius = 20.0;
-    let fahrenheit = celsius_to_fahrenheit(celsius);
-    println!("{}°C is {}°F", celsius, fahrenheit);
-}
