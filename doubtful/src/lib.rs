b18e6c0f739fa48013c1a78f7dc972c277ad60d9
abrakingoo <maikoabraham68@gmail.com>
2025-04-02 21:22:05 +0300

    lib: doubtful

---
+++ doubtful/src/lib.rs
@@ -0,0 +1,5 @@
+// a function named doubtful which appends a question mark to every string passed to it. 
+// It must not return a value.
+pub fn doubtful(s: &mut String ) {
+    s.push_str("?");
+}
