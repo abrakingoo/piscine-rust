a136e8a7c7a9afb88c47e1dec3c9d0ecbb0f06cb
abrakingoo <maikoabraham68@gmail.com>
2025-04-02 21:01:54 +0300

    lib: borrow

--- borrow/src/lib.rs
+++ borrow/src/lib.rs
@@ -1,3 +1,6 @@
+// Create a function named str_len, you'll need to complete the function signature. 
+// Your function should accept a borrowed string, and return its length (in characters).
+
 pub fn str_len(s: &str ) -> usize {
-    s.len()
+    s.chars().count()
 }
