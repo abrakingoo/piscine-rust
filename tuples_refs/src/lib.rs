6c09cc0f80b8ef4f36b89ffba8af3db0ae72296d
abrakingoo <maikoabraham68@gmail.com>
2025-04-02 19:58:40 +0300

    lib: tuples_refs

--- tuples_refs/src/lib.rs
+++ tuples_refs/src/lib.rs
@@ -1,14 +1,18 @@
 #[derive(Debug, PartialEq)]
-pub struct student(pub u32, pub String, pub String)
 
+// Define a tuple struct to represent a Student. Each is identified by an id of type u32, their first name and last name.
+pub struct Student(pub u32, pub String, pub String)
+
+
+// functions to return the id, first name and last name.
 pub fn id(student: &Student) -> u32 {
     student.0
 }
 
 pub fn first_name(student: &Student) -> &str {
-    student.1[..]
+    student.1
 }
 
 pub fn last_name(student: &Student) -> &str {
-    student.2[..]
+    student.2
 }
