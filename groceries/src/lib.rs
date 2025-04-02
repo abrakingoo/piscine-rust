d12f89174311f025c28898879847d10fdcf0deda
abrakingoo <maikoabraham68@gmail.com>
2025-04-02 07:43:41 +0300

    lib: groceries

--- groceries/src/lib.rs
+++ groceries/src/lib.rs
@@ -1,10 +1,10 @@
 // inserts a new element at the end of the Vec.
 pub fn insert(vec: &mut Vec<String>, val: String) {
-    vec.push(val);
+    vec.push(val)
 }
 
 // returns the value found at the index passed as an argument.
 pub fn at_index(slice: &[String], index: usize) -> &str {
-    let res = &slice[index];
-    res.to_string();
+    let res = slice[index];
+    &res[..]
 }
