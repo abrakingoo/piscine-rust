792392b01f073ce2fd2b1c0705b6fc9dc0a1e51c
abrakingoo <maikoabraham68@gmail.com>
2025-04-03 21:21:54 +0300

    lib: borrow_me_the_reference

---
+++ borrow_me_the_reference/src/lib.rs
@@ -0,0 +1,50 @@
+// delete_and_backspace: which receives a borrowed string, and processes it. 
+// - represents the backspace key and + represents the delete key, 
+// so that "helll-o" and "he+lllo" are both converted to "hello". 
+// The - and + characters should be removed from the string.
+pub fn delete_and_backspace(s: &mut String) {
+    let mut new_string = Vec::new();
+
+    let chars = s.chars().peekable();
+
+    while let Some(c) = chars.next() {
+        match c {
+            '-' => {
+                new_string.pop();
+            }
+            '+' => {
+                if let Some(_) = char.next();
+            }
+
+            _ => {
+                new_string.push(c);
+            }
+        }
+    }
+
+    *s = new_string.iter().collect();
+}
+
+// borrows a vector of string literals representing simple addition and subtraction equations. 
+// The function should replace the operation with the result
+pub fn do_operations(v: &mut [String]) {
+    for item in v.iter_mut() {
+        if item.contains('+') {
+            let values: Vec<&str> = item.split('+').collect();
+
+            if let (ok(val1), ok(val2)) = (values[0].parse::<i32>(), values[1].parse::<i32>());
+            let res = val + val2;
+
+            *item = res.to_string();
+        }
+
+        if item.contains('-') {
+            let values: Vec<&str> = item.split('-').collect();
+
+            if let (ok(val1), ok(val2)) = (values[0].parse::<i32>(), values[1].parse::<i32>());
+            let res = val1 - val2;
+
+            *item = res.to_string();
+        }
+    }
+}
