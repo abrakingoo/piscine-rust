4ba2224e1886093f9314a8847adabad294d3c915
abrakingoo <maikoabraham68@gmail.com>
2025-04-08 21:54:48 +0300

    lib: unwrap

--- unwrap_or_expect/src/lib.rs
+++ unwrap_or_expect/src/lib.rs
@@ -21,7 +21,7 @@ pub enum Security {
 
 pub fn fetch_data(server: Result<&str, &str>, security_level: Security) -> String  {
     match (server, security_level) {
-        (Ok(url), _) => url,
+        (Ok(url), _) => url.to_string(),
 
         (Err(msg), Security::Unknown) => panic!(),
         (Err(_), Security::Message) => panic!("ERROR: program stops"),
