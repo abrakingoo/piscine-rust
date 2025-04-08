9fb4db40f588c4d982377e259b89cf7fa726cfbe
abrakingoo <maikoabraham68@gmail.com>
2025-04-08 22:23:35 +0300

    lib: unwrap

--- unwrap_or_expect/src/lib.rs
+++ unwrap_or_expect/src/lib.rs
@@ -21,11 +21,11 @@ pub enum Security {
 
 pub fn fetch_data(server: Result<&str, &str>, security_level: Security) -> String  {
     match security_level {
-        Security::Unknown => server.to_string(),
-        Security::Message => server.unwrap_or("ERROR: program stops".to_string()),
+        Security::Unknown => server.unwrap(),
+        Security::Message => server.expect("ERROR: program stops".to_string()),
         Security::Warning => server.unwrap_or("WARNING: check the server".to_string()),
         Security::NotFound => server.unwrap_or_else(|err| format!("Not Found: {}", err)),
-        Security::UnexpectedUrl => server.unwrap_or_else(server.to_string())
+        Security::UnexpectedUrl => server.unwrap_err()
 
     }
 }
