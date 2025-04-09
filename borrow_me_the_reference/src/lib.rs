3a4e7fa5a2ffebae53f7c2738f1dd42d0fa10c8b
abrakingoo <maikoabraham68@gmail.com>
2025-04-09 10:29:21 +0300

    stashed

---
+++ borrow_me_the_reference/src/lib.rs
@@ -0,0 +1,9 @@
+pub fn delete_and_backspace(s: &mut String) {
+    let mut new_str = String::new();
+    let original_string = s;
+    for c in original_string.chars(){
+        if c == '-' {
+            
+        }
+    };
+}
