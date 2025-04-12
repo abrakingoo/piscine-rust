72a0278bb9aa96c50e4b181bf9309b94b057daaa
abrakingoo <maikoabraham68@gmail.com>
2025-04-12 07:11:22 +0300

    lib:: starts

--- stars/src/lib.rs
+++ stars/src/lib.rs
@@ -1,10 +1,4 @@
 pub fn stars(n: u32) -> String {
-    let end = n*2;
-    let mut res = String::new();
-
-    for _ in 1..=end {
-        res += "*";
-    }
-
-    res
+    let count = 2u32.pow(n);
+    "*".repeat(count as usize)
 }
